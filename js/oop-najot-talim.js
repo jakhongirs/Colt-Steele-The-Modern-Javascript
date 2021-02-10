fe81090915f921d7e777d45a702fc2a75c2d14f2
jakhongirs <j.sagdullaev31@gmail.com>
2021-02-10 11:50:45 +0500

    OOP H/W

--- js/oop-najot-talim.js
+++ js/oop-najot-talim.js
@@ -1,4 +1,4 @@
-class Calc {
+/* class Calc {
   x
   y
 
@@ -19,4 +19,32 @@ class Calc {
 const calc = new Calc(20, 10)
 
 console.log(calc.add());
-console.log(calc.subtract());
+console.log(calc.subtract()); */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+class Profession {
+  name
+  #salary
+  job
+
+
+  constructor(name, salary, job){
+    this.name = name
+    this.#salary = salary
+    this.job = job
+  }
+
+  get getInfo() {
+    return `${this.name + ' ' +  this.#salary + ' ' + this.job}`
+  }
+
+}
+
+const developer = new Profession('Jakhongir', 500, 'developer')
+const doctor = new Profession('Husayn', 1500, 'doctor')
+const designer = new Profession('Bahodir', 800, 'designer')
+
+console.log(developer.getInfo);
+console.log(doctor.getInfo);
+console.log(designer.getInfo);
