35732e9e03aced85042fdfbf8d30811d9b663d98
jakhongirs <j.sagdullaev31@gmail.com>
2021-03-03 21:15:48 +0500

    Functions

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+/* -------------------------------------------------------------------------------------------------------- */
+// Our first function:
+
+/* function funcName(){
+  console.log("Assalomu Alaykum");
+  console.log("Hello");
+  console.log("Здравствуйте");
+}
+
+funcName()
+funcName()
+funcName() */
+/* -------------------------------------------------------------------------------------------------------- */
+// Dice Roll:
+
+/* function diceRoll() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  console.log(`Rolled: ${roll}`);
+}
+
+function throwDice(){
+  diceRoll();
+  diceRoll();
+  diceRoll();
+  diceRoll();
+  diceRoll();
+  diceRoll();
+}
+
+throwDice(); */
+/* -------------------------------------------------------------------------------------------------------- */
+
